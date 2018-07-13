d3d1f1a8279ecd324f488c2d9b76a0d76f9a9633
Matheus <matheusov@gmail.com>
2018-07-13 12:09:49 -0300

    Updating download url

--- download_data.sh
+++ download_data.sh
@@ -5,6 +5,6 @@ mkdir -p downloads
 
 for ANO in {2016..2017}; do
 	for MES in {01..12}; do
-		wget -O downloads/$ANO$MES.zip -o logs/downloads/$ANO$MES.log "http://arquivos.portaldatransparencia.gov.br/downloads.asp?a=$ANO&m=$MES&consulta=BolsaFamiliaFolhaPagamento"
+		wget -O downloads/$ANO$MES.zip -o logs/downloads/$ANO$MES.log "http://www.portaldatransparencia.gov.br/download-de-dados/bolsa-familia-pagamentos/$ANO$MES"
 	done
 done
