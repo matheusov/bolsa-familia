4fd1e150bf87e1437e69fee9b93a440501605f4e
Matheus <matheusov@gmail.com>
2018-07-22 20:09:14 -0300

    Download only one year

--- download_data.sh
+++ download_data.sh
@@ -3,7 +3,7 @@
 mkdir -p logs/downloads
 mkdir -p downloads
 
-for ANO in {2016..2017}; do
+for ANO in {2017..2017}; do
 	for MES in {01..12}; do
 		wget -O downloads/$ANO$MES.zip -o logs/downloads/$ANO$MES.log "http://www.portaldatransparencia.gov.br/download-de-dados/bolsa-familia-pagamentos/$ANO$MES"
 	done
